a4b41034a2749b441e72d6c13f7d1212a0fd2266
YukiEndo <yuki.a.endo@imjp.co.jp>
2020-01-11 23:33:00 +0900

    [add]MongoDB、mongoose
    MongoDB+mongoose+Apolloの接続テスト

--- 01/src/server/resolvers.js
+++ 01/src/server/resolvers.js
@@ -1,17 +1,42 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/learn-graphql-01', { useNewUrlParser: true, useUnifiedTopology: true });
+const database = mongoose.connection;
+
+database.on('error', console.error.bind(console, '接続エラー'));
+database.once('open', () => {
+  console.log('接続');
+});
+
+const ToDoSchema = new mongoose.Schema(
+  {
+    id: Number,
+    title: String,
+    description: String,
+    deadline: String,
+    isComplete: Boolean
+  },
+  { collection: 'ToDos' }
+);
+const ToDo = mongoose.model('ToDo', ToDoSchema);
+
 const dummyToDos = [];
 for (let i = 0; i < 10; i++) {
   dummyToDos.push({
     id: i,
-    title: "タイトル" + i,
-    description: "説明" + i,
-    deadline: "2020-01-01",
+    title: 'タイトル' + i,
+    description: '説明' + i,
+    deadline: '2020-01-01',
     isComplete: false
   });
 }
 
 const resolvers = {
   Query: {
-    todos: () => dummyToDos
+    todos: async () => {
+      const todos = await ToDo.find();
+
+      return todos;
+    }
   }
 };
 
