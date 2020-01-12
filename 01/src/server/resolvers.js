f631972f8a1240693c3010b256412ddfcecc9c25
YukiEndo <yuki.a.endo@imjp.co.jp>
2020-01-12 15:47:04 +0900

    [remove]mongodb設定ファイル

--- 01/src/server/resolvers.js
+++ 01/src/server/resolvers.js
@@ -19,17 +19,6 @@ const ToDoSchema = new mongoose.Schema(
 );
 const ToDo = mongoose.model('ToDo', ToDoSchema);
 
-const dummyToDos = [];
-for (let i = 0; i < 10; i++) {
-  dummyToDos.push({
-    id: i,
-    title: 'タイトル' + i,
-    description: '説明' + i,
-    deadline: '2020-01-01',
-    isComplete: false
-  });
-}
-
 const resolvers = {
   Query: {
     todos: async () => {
